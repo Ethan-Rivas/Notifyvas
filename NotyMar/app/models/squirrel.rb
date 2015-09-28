a61a2403af9a8fa7f0a20d4c9b1829d399adcacc
Ethan <ethan-rivas@hotmail.com>
2015-09-28 01:22:39 -0500

    Creation of friendship model with his route & configuration from Squirrels model

--- NotyMar/app/models/squirrel.rb
+++ NotyMar/app/models/squirrel.rb
@@ -3,4 +3,9 @@ class Squirrel < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :back_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :back_friends, :through => :back_friendships, :source => :squirrel
 end
